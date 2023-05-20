13b1e7ea8859a7a3d0bb360455484bfeba688b27
Ryohei <katsume@gmail.com>
2023-05-20 14:59:34 +0900

    add google authenticate

--- ktor-appengine-util/src/main/kotlin/net/sunaba/ktor/auth/google_sign_in.kt
+++ ktor-appengine-util/src/main/kotlin/net/sunaba/ktor/auth/google_sign_in.kt
@@ -2,104 +2,50 @@ package net.sunaba.ktor.auth
 
 import com.auth0.jwk.GuavaCachedJwkProvider
 import com.auth0.jwk.UrlJwkProvider
-import io.ktor.server.application.ApplicationCall
-import io.ktor.server.application.call
-import io.ktor.server.auth.authenticate
-import io.ktor.server.auth.authentication
-import io.ktor.server.auth.jwt.JWTPrincipal
-import io.ktor.server.auth.jwt.jwt
-import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.response.respond
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.Routing
-import io.ktor.server.routing.get
-import io.ktor.server.routing.post
+import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import java.net.URL
 
-fun configureGoogleSignIn(clientId:String, configure:GoogleSignInConfig.()->Unit) = GoogleSignInConfig(clientId).apply(configure)
+class GoogleSignInConfig internal constructor() {
+    internal var challenge: JWTAuthChallengeFunction = { s: String, s1: String -> }
+    internal var authenticationFunction: AuthenticationFunction<JWTCredential> =
+        { jwtCredential -> JWTPrincipal(jwtCredential.payload) }
+    internal var additionalVerifier: JWTConfigureFunction = {}
 
-class GoogleSignInConfig internal constructor(val clientId: String) {
-    var authName: String? = "google"
-    var loginPath: String = "/__login"
-    var onLoginAction: ApplicationCall.(jwt: JWTPrincipal) -> Boolean = { true }
+    /**
+     * Specifies what to send back if JWT authentication fails.
+     */
+    fun challenge(block: JWTAuthChallengeFunction) {
+        challenge = block
+    }
+
+    /**
+     * Allows you to perform additional validations on the JWT payload.
+     * @return a principal (usually an instance of [JWTPrincipal]) or `null`
+     */
+    fun validate(validate: suspend ApplicationCall.(JWTCredential) -> Principal?) {
+        authenticationFunction = validate
+    }
+
+    fun additionalVerifier(configure: JWTConfigureFunction = {}) {
+        this.additionalVerifier = configure
+    }
 }
 
-fun AuthenticationConfig.googleSignIn(config: GoogleSignInConfig) {
-    val audience = config.clientId
+fun AuthenticationConfig.googleSignIn(name: String?=null, clientId: String, configure: GoogleSignInConfig.() -> Unit = {}) {
     val jwkProvider = UrlJwkProvider(URL("https://www.googleapis.com/oauth2/v3/certs"))
     val cachedJwkProvider = GuavaCachedJwkProvider(jwkProvider)
-    jwt(config.authName) {
+
+    val config: GoogleSignInConfig = GoogleSignInConfig().apply(configure)
+
+    jwt(name) {
         verifier(cachedJwkProvider) {
             withIssuer("accounts.google.com", "https://accounts.google.com")
-            withAudience(audience)
+            withAudience(clientId)
+            config.additionalVerifier.invoke(this)
         }
-        validate { jwtCredential -> JWTPrincipal(jwtCredential.payload) }
+        validate(config.authenticationFunction)
+        challenge(config.challenge)
     }
 }
-
-fun Routing.installSimpleLogin(config: GoogleSignInConfig) {
-
-    get(config.loginPath) {
-        call.respondText("""<html lang="en">
-<head>
-    <meta content="width=device-width, initial-scale=1, minimum-scale=1" name="viewport">
-</head>
-<body>
-<script src="https://accounts.google.com/gsi/client" async defer></script>
-<div id="g_id_onload"
-     data-client_id="${config.clientId}"
-     data-context="signin"
-     data-ux_mode="popup"
-     data-callback="onSignIn"
-     data-auto_select="true"
-     data-itp_support="true">
-</div>
-
-<div class="g_id_signin"
-     data-type="standard"
-     data-shape="rectangular"
-     data-theme="outline"
-     data-text="signin_with"
-     data-size="large"
-     data-logo_alignment="left">
-</div>
-<script>
-  function onSignIn(response) {
-     // decodeJwtResponse() is a custom function defined by you
-     // to decode the credential response.
-             // The ID token you need to pass to your backend:
-        var id_token = response.credential
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '${config.loginPath}');
-        xhr.setRequestHeader('Authorization', 'Bearer ' + id_token)
-        xhr.onload = function () {
-            if (xhr.status == 200) {
-                var searchParams = new URLSearchParams(window.location.search)
-                var continueUrl = searchParams.get("continue")
-                if (continueUrl) {
-                    window.location = continueUrl
-                }
-            }
-        };
-        xhr.send();
-  }
-</script>
-</body>
-</html>""", ContentType.Text.Html)
-    }
-
-
-    authenticate(config.authName) {
-        post(config.loginPath) {
-            //Verify the Google ID token on your server side
-            //https://developers.google.com/identity/gsi/web/guides/verify-google-id-token
-            val jwtPrincipal = call.authentication.principal<JWTPrincipal>()!!
-            config.onLoginAction.invoke(this.call, jwtPrincipal)
-            call.respond(HttpStatusCode.OK)
-        }
-    }
-
-}
-
--- samples/sample-appengine/src/main/kotlin/net/sunaba/plugins/Routing.kt
+++ samples/sample-appengine/src/main/kotlin/net/sunaba/plugins/Routing.kt
@@ -5,8 +5,10 @@ import com.google.cloud.tasks.v2.CloudTasksClient
 import com.google.cloud.tasks.v2.HttpMethod
 import com.google.cloud.tasks.v2.QueueName
 import com.google.cloud.tasks.v2.Task
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import net.sunaba.ktor.auth.*
@@ -15,16 +17,15 @@ import java.lang.Exception
 
 fun Application.configureRouting() {
 
-    val config = configureGoogleSignIn("") {}
+    val clientId:String = ""
 
     install(Authentication) {
-        googleSignIn(config)
+        googleSignIn("google", clientId)
         appengineCron("cron")
         appengineTaskQueue("tq")
     }
 
     routing {
-        installSimpleLogin(config)
 
         get("/") {
             call.respondText("Hello World!")
@@ -60,10 +61,77 @@ fun Application.configureRouting() {
         }
 
 
-//        authenticate() {
-//            get("/need_login") {
-//                call.respondText("Hello World!")
-//            }
-//        }
+        googleSignInRoutes("/login", clientId, "google")
+
+        authenticate("google") {
+            get("/need_login") {
+                call.respondText("Hello World!")
+            }
+        }
     }
 }
+
+
+fun Routing.googleSignInRoutes(loginPath: String, clientId: String, authProviderName: String, ) {
+
+    get(loginPath) {
+        call.respondText(
+            """<html lang="en">
+<head>
+    <meta content="width=device-width, initial-scale=1, minimum-scale=1" name="viewport">
+</head>
+<body>
+<script src="https://accounts.google.com/gsi/client" async defer></script>
+<div id="g_id_onload"
+     data-client_id="${clientId}"
+     data-context="signin"
+     data-ux_mode="popup"
+     data-callback="onSignIn"
+     data-auto_select="true"
+     data-itp_support="true">
+</div>
+
+<div class="g_id_signin"
+     data-type="standard"
+     data-shape="rectangular"
+     data-theme="outline"
+     data-text="signin_with"
+     data-size="large"
+     data-logo_alignment="left">
+</div>
+<script>
+  function onSignIn(response) {
+     // decodeJwtResponse() is a custom function defined by you
+     // to decode the credential response.
+             // The ID token you need to pass to your backend:
+        var id_token = response.credential
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '${loginPath}');
+        xhr.setRequestHeader('Authorization', 'Bearer ' + id_token)
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                var searchParams = new URLSearchParams(window.location.search)
+                var continueUrl = searchParams.get("continue")
+                if (continueUrl) {
+                    window.location = continueUrl
+                }
+            }
+        };
+        xhr.send();
+  }
+</script>
+</body>
+</html>""", ContentType.Text.Html
+        )
+    }
+
+    authenticate(authProviderName) {
+        post(loginPath) {
+            val jwtPrincipal = call.authentication.principal<JWTPrincipal>()!!
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+
+}
+
+
